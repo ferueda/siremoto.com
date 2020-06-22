7dcb384281f53f919b75d4a061a6b9d5c99b2f4c
ferueda <fe.rueda@outlook.com>
2020-06-22 18:40:09 -0400

    added a Container shared component and structured the App component with the newly added components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import GlobalStyle from './styles/GlobalStyle';
+import Header from './components/Header';
 
 function App() {
 	return (
-		<div>
-			<h1>caca</h1>
-		</div>
+		<React.Fragment>
+			<GlobalStyle />
+			<Header />
+		</React.Fragment>
 	);
 }
 
