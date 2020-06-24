3f196a796a3230767cce667a3d1fdf72e5049d01
ferueda <fe.rueda@outlook.com>
2020-06-23 23:04:33 -0400

    created the jobs reducer function and the actions object

---
+++ src/context/reducers.js
@@ -0,0 +1,18 @@
+export const actions = {
+	FETCH_INIT: 'FETCH_INIT',
+	FETCH_SUCCESS: 'FETCH_SUCCESS',
+	FETCH_ERROR: 'FETCH_ERROR',
+};
+
+export const jobsReducer = (state, action) => {
+	switch (action.type) {
+		case actions.FETCH_INIT:
+			return { ...state, isLoading: true, isError: false };
+		case actions.FETCH_SUCCESS:
+			return { ...state, jobs: action.payload, isError: false, isLoading: false };
+		case actions.FETCH_ERROR:
+			return { ...state, isLoading: false, isError: true };
+		default:
+			return state;
+	}
+};
