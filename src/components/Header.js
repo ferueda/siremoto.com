d1ac69ca12ef165cb00dddb9c8f08962711effd8
ferueda <fe.rueda@outlook.com>
2020-06-23 23:07:47 -0400

    changed the background image color gradient, implemented the app colors through the gloval vars, incresed the Title font size, and changed the title content

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,21 +9,20 @@ const HeroContainer = styled(Container)`
 	flex-direction: column;
 	align-items: center;
 	justify-content: space-evenly;
-
-	background-image: linear-gradient(rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.6)),
+	background-image: linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.6)),
 		url('/assets/images/laptop-stickers-640x427.jpg');
 	background-position: center;
 	background-repeat: no-repeat;
 	background-size: cover;
-
 	min-height: 45vh;
 `;
 
 const Title = styled.div`
+	margin-top: -3rem;
 	h1 {
-		color: var(--main-orange);
+		color: var(--main-color);
 		font-weight: 700;
-		font-size: 2.5rem;
+		font-size: 3.5rem;
 		margin-bottom: 0.5rem;
 
 		a {
@@ -50,7 +49,7 @@ const Header = () => {
 			<HeroContainer>
 				<Title>
 					<h1>
-						<a href="/">Trabajos Remotos</a>
+						<a href="/">siRemoto</a>
 					</h1>
 					<h2>Encuentra los últimos trabajos remotos en tecnología y programación</h2>
 				</Title>
