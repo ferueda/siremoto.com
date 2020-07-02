c7bcb483f7a62ef4f3422ed73c02b3fa536689e0
ferueda <fe.rueda@outlook.com>
2020-07-01 23:10:10 -0400

    created a component that renders the flag svg icons

---
+++ src/components/FlagIcon.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledImg = styled.img`
+	width: 15px;
+	height: 15px;
+	margin: auto 0.25em auto 0;
+`;
+
+const country = {
+	argentina: 'argentina',
+	bolivia: 'bolivia',
+	canada: 'canada',
+	chile: 'chile',
+	colombia: 'colombia',
+	'costa rica': 'costa-rica',
+	cuba: 'cuba',
+	ecuador: 'ecuador',
+	inglaterra: 'england',
+	españa: 'spain',
+	honduras: 'honduras',
+	méxico: 'mexico',
+	panama: 'panama',
+	paraguay: 'paraguay',
+	perú: 'peru',
+	remote: 'remote',
+	uruguay: 'uruguay',
+	'estados unidos': 'usa',
+	venezuela: 'venezuela',
+};
+
+const FlagIcon = ({ flag }) => {
+	return <StyledImg src={`/assets/flags/${country[flag]}.svg`} alt={`${flag} flag`} />;
+};
+
+export default FlagIcon;
