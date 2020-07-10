f669f6c52ebdaa582c2d7cd417854f4584a30a24
ferueda <fe.rueda@outlook.com>
2020-07-09 22:28:35 -0400

    add style to JobDescription.js added basic styling to JobDescription component

--- src/components/JobDescription.js
+++ src/components/JobDescription.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import Button from './Button';
+
+const Container = styled.div`
+	margin: 0 auto;
+	padding: 2rem 1rem;
+
+	p {
+		text-align: justify;
+	}
+`;
 
 const JobDescription = ({ description, apply }) => {
-	const { job_title, content_html } = description;
+	const { content_html } = description;
+	console.log(content_html);
 	return (
-		<div>
-			<h2>{job_title}</h2>
+		<Container>
+			<Button url={apply}>Postular ahora</Button>
 			<div dangerouslySetInnerHTML={{ __html: content_html }}></div>
-		</div>
+			<Button url={apply}>Postular ahora</Button>
+		</Container>
 	);
 };
 
