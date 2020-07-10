10f516799a1da9a3b37eab6ab38b0d985c4dac8f
ferueda <fe.rueda@outlook.com>
2020-07-09 22:30:27 -0400

    refactor ThemeContext.js added memoization to the ThemeContext component

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -3,13 +3,17 @@ import React, { createContext } from 'react';
 export const ThemeContext = createContext();
 
 const ThemeContextProvider = ({ children }) => {
-	const contextObject = {
-		light: true,
-		lightBackgroundImageSrc: '/assets/images/laptop-stickers-640x427.jpg',
-		darkBackgroundImageSrc: '/assets/images/street-desktop-640x427.jpg',
-	};
+	const contextObject = React.useMemo(() => {
+		return {
+			light: true,
+			lightBackgroundImageSrc: '/assets/images/laptop-stickers-640x427.jpg',
+			darkBackgroundImageSrc: '/assets/images/street-desktop-640x427.jpg',
+		};
+	}, []);
+
+	console.log('ThemeContext');
 
 	return <ThemeContext.Provider value={{ ...contextObject }}>{children}</ThemeContext.Provider>;
 };
 
-export default ThemeContextProvider;
+export default React.memo(ThemeContextProvider);
